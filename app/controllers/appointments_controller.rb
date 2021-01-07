fc8eb56270479508de3ccedb9e51a37a234eba17
Brian Li <Brian.Li96@yahoo.com>
2021-01-07 11:58:08 -0600

    Project2

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -6,10 +6,12 @@ class AppointmentsController < ApplicationController
 
     def new 
         @appointment = Appointment.new
+        @animal_id = params[:appointment][:animal_id]
     end
 
     def create
-        @appointment = Appointment.new(date: params[:appointment][:date], time: params[:appointment][:time], shelter_id: 1, person_id: params[:appointment][:person_id], animal_id: params[:appointment][:animal_id])        if @appointment.save
+        @appointment = Appointment.new(date: params[:appointment][:date], time: params[:appointment][:time], shelter_id: 1, person_id: params[:appointment][:person_id], animal_id: params[:appointment][:animal_id])        
+        if @appointment.save
             redirect_to person_path(@appointment.person)
         else
             render new_appointment_path
