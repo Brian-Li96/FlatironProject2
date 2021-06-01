3a9f226f9d06093efcf8751004e440039e320eea
github username <avelonpang@gmail.com>
2021-05-31 19:39:40 -0500

    updated

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -4,11 +4,5 @@ belongs_to :person, optional: true
 belongs_to :shelter, optional: true
 belongs_to :animal
 
-# def self.one_app
-#     #animal cannot have same app time on same date
-#     Appointment.all.select do |app|
-#     app.animal.date == false
-#     end 
-#     end
 
 end
